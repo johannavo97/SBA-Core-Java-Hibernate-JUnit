48a86a1f432a6f0c367cf5cda83641c43d2597e2
Johanna Vo <thanhtruc2009@gmail.com>
2022-05-18 08:51:09 -0700

    update model classes

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,32 @@
 package sba.sms.models;
 
-public class Course {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NamedQuery(name = "findStudentCourses", query = "from Course where student = :s")
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @NonNull
+    @Column(length = 50, nullable = false)
+    String name;
+    @NonNull
+    @Column(length = 50, nullable = false)
+    String instructor;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "courses", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    List<Student> students = new ArrayList<>();
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,36 @@
 package sba.sms.models;
 
-public class Student {
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @NonNull
+    @Column(length = 50, unique = true,name = "email")
+    String email;
+    @NonNull
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+    @NonNull
+    @Column(length = 50, nullable = false, name = "password")
+    String password;
+
+    @ToString.Exclude
+    @ManyToMany( fetch = FetchType.EAGER,cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinTable(name = "student_course",
+            joinColumns = @JoinColumn(name = "student_email"), inverseJoinColumns = @JoinColumn(name = "courses_id"))
+    List<Course> courses = new ArrayList<>();
 }
+
