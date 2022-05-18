fbae8012fb190020159764e434cc364c1cf30c18
Johanna Vo <thanhtruc2009@gmail.com>
2022-05-18 09:00:13 -0700

    update services

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -1,8 +1,55 @@
 package sba.sms.services;
 
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.List;
 
-public class CourseService  {
+public class CourseService implements CourseI {
 
+    @Override
+    public void createCourse(Course course) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = s.beginTransaction();
+            s.persist(course);
+            tx.commit();
+        } catch (HibernateException exception) {
+            if (tx!=null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+    }
+
+    @Override
+    public Course getCourseById(int courseId) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            Course c = s.get(Course.class,courseId);
+            if(c == null)
+                throw new HibernateException("Did not find Course");
+            else
+                return c;
+
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+        return new Course();
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Course> c = s.createQuery("from Course",Course.class).getResultList();
+        s.close();
+        return c;
+    }
 }
--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,5 +1,84 @@
 package sba.sms.services;
 
-public class StudentService  {
+import jakarta.persistence.TypedQuery;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentService implements StudentI {
+
+    @Override
+    public List<Student> getAllStudents() {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        List<Student> students = s.createQuery("from Student",Student.class).getResultList();
+        s.close();
+        return students;
+    }
+
+    @Override
+    public void createStudent(Student student) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = s.beginTransaction();
+            s.persist(student);
+            tx.commit();
+        } catch (HibernateException exception) {
+            if (tx!=null) tx.rollback();
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+    }
+
+    @Override
+    public Student getStudentByEmail(String email) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            Student e = s.get(Student.class,email);
+            if(e == null)
+                throw new HibernateException("Did not find Student");
+            else
+                return e;
+
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+        return new Student();
+    }
+
+    @Override
+    public boolean validateStudent(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public void registerStudentToCourse(String email, int courseId) {
+
+    }
+
+    @Override
+    public List<Course> getStudentCourses(String email) {
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = s.beginTransaction();
+            TypedQuery<Course> q = s.createNamedQuery("findStudentCourses");
+            q.setParameter("s", s);
+            return q.getResultList();
+
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        } finally {
+            s.close();
+        }
+        return new ArrayList<>();
+    }
 }
