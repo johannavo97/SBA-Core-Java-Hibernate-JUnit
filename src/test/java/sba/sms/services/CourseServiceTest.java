729db353913339966bf6c815cb97ffcdcdf81cb3
Johanna Vo <thanhtruc2009@gmail.com>
2022-05-25 14:00:40 -0700

    test updated

--- src/test/java/sba/sms/services/CourseServiceTest.java
+++ src/test/java/sba/sms/services/CourseServiceTest.java
@@ -1,7 +1,10 @@
 package sba.sms.services;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import sba.sms.utils.CommandLine;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CourseServiceTest {
     static CourseService courseService;
@@ -11,5 +14,10 @@ public class CourseServiceTest {
         CommandLine.addData();
     }
 
-
+    @Test
+    void getCourseById() {
+        assertThat(courseService.getCourseById(1)).extracting(course -> course.getId()).isEqualTo(1);
+        assertThat(courseService.getCourseById(1)).extracting(course -> course.getName()).isEqualTo("Java");
+        assertThat(courseService.getCourseById(1)).extracting(course -> course.getInstructor()).isEqualTo("Phillip Witkin");
+    }
 }
